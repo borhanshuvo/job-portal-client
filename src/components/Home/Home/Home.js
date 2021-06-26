9ad82c4bc80f3608d3d4e9e698aae0a8f9030059
borhanshuvo <borhan015@gmail.com>
2021-06-26 22:00:43 +0600

    changes

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,14 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react';
-import ReactPaginate from 'react-paginate';
 import { useHistory } from 'react-router-dom';
 import { UserContext } from '../../../App';
 import Navbar from '../../Navbar/Navbar';
+import Pagination from '../Pagination/Pagination';
 import './Home.css';
 
 const Home = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     const [allJobPost, setAllJobPost] = useState([]);
-    const [pageNumber, setPageNumber] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(20);
+    // const [pageNumber, setPageNumber] = useState(0);
     const active = 'active';
     const history = useHistory();
 
@@ -20,33 +22,10 @@ const Home = () => {
             });
     }, []);
 
-    const postPerPage = 20;
-    const pagesVisited = pageNumber * postPerPage;
-
-    const displayPosts = allJobPost
-        .slice(pagesVisited, pagesVisited + postPerPage)
-        .map((post) => {
-            return (
-                <div className="col-md-12 pb-5">
-                    <div className="card">
-                        <div className="card-body text-center">
-                            <h4 className="card-title">{post.companyName}</h4>
-                            <p className="card-text">Company Email : {post.companyEmail}</p>
-                            <p className="card-text">Job Description : {post.description}</p>
-                            <p className="card-text">Vacancy : {post.vacancy}</p>
-                        </div>
-                        <div className="card-footer text-center">
-                            <button className="btn btn-primary" onClick={() => applyId(post._id)}>Apply Now</button>
-                        </div>
-                    </div>
-                </div>
-            );
-        });
-
-    const pageCount = Math.ceil(allJobPost.length / postPerPage);
-    const changePage = ({ selected }) => {
-        setPageNumber(selected);
-    }
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = allJobPost.slice(indexOfFirstPost, indexOfLastPost);
+    const paginate = pageNumber => setCurrentPage(pageNumber);
 
     const applyId = (id) => {
         const url = `dashboard/applyJob/${id}`;
@@ -59,19 +38,25 @@ const Home = () => {
             {loggedInUser.email &&
                 <div className="container pt-5 mt-5">
                     <div className="row">
-                        {displayPosts}
-                        <div className="ps-5 ms-5">
-                            <ReactPaginate
-                                previousLabel={"Previous"}
-                                nextLabel={"Next"}
-                                pageCount={pageCount}
-                                onPageChange={changePage}
-                                containerClassName={"paginationBttns"}
-                                previousLinkClassName={"previousBttn"}
-                                nextLinkClassName={"nextBttn"}
-                                disabledClassName={"paginationDisabled"}
-                                activeLinkClassName={"paginationActive"}
-                            />
+                        {
+                            currentPosts.map(post =>
+                                <div className="col-md-12 pb-5">
+                                    <div className="card">
+                                        <div className="card-body text-center">
+                                            <h4 className="card-title">{post.companyName}</h4>
+                                            <p className="card-text">Company Email : {post.companyEmail}</p>
+                                            <p className="card-text">Job Description : {post.description}</p>
+                                            <p className="card-text">Vacancy : {post.vacancy}</p>
+                                        </div>
+                                        <div className="card-footer text-center">
+                                            <button className="btn btn-primary" onClick={() => applyId(post._id)}>Apply Now</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }
+                        <div className="d-flex justify-content-center mt-4">
+                            <Pagination postsPerPage={postsPerPage} totalPosts={allJobPost.length} paginate={paginate} />
                         </div>
                     </div>
                 </div>
